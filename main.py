01dcd36f01ed07be2577a6230d8945591f152b88
Yordy Almonte <yoalmonte.dev@outlook.com>
2024-04-16 15:19:20 -0400

    🤔 Third Commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 
 app = FastAPI(
@@ -6,6 +7,30 @@ app = FastAPI(
   version = "0.0.1",
 )
 
-@app.get("/", tags=["Home"])
+movies = [
+  {
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	}
+]
+
+@app.get("/", tags = ["Home"])
 def message():
-  return "Hello World!"
+  return HTMLResponse('''
+    <html>
+      <head>
+        <title>My First FastAPI</title>
+      </head>
+      <body>
+        <h1>Hello World</h1>
+      </body>
+    </html>
+  ''')
+
+@app.get("/movies", tags = ['Movies'])
+def get_movies():
+  return movies
