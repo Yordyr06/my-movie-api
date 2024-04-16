5399055dc49e5263716cbeaf227dda85384d901c
Yordy Almonte <yoalmonte.dev@outlook.com>
2024-04-16 15:06:22 -0400

    🤔 Second Commit

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI()
 
-@app.get("/")
+app = FastAPI(
+  title = "My First FastAPI",
+  version = "0.0.1",
+)
+
+@app.get("/", tags=["Home"])
 def message():
   return "Hello World!"
